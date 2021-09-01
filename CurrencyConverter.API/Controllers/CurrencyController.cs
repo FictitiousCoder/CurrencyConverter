e7c56304cbfccf38b62ea8189e30e03aff315d35
unknown <jonny_b@hotmail.no>
2021-09-01 17:23:56 +0200

    feat: Add currency API
    * Added a web API project in the presentation layer.
    * Added a controller and an endpoint for getting the converted sum of a base currency.

---
+++ CurrencyConverter.API/Controllers/CurrencyController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using CurrencyConvert.Infrastructure.Models;
+using CurrencyConvert.Infrastructure.Services;
+using CurrencyConverter.Infrastructure.RelationalStorage;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CurrencyConverter.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CurrencyController : ControllerBase
+    {
+        private readonly ICurrencyService _currencyService;
+        private readonly ExchangeRateDbContext _exchangeRateDbContext;
+        private readonly ILogger<CurrencyController> _logger;
+
+        public CurrencyController(ICurrencyService currencyService, ExchangeRateDbContext exchangeRateDbContext, ILogger<CurrencyController> logger)
+        {
+            _currencyService = currencyService;
+            _exchangeRateDbContext = exchangeRateDbContext;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("convert")]
+        public async Task<IActionResult> ConvertCurrency([FromQuery] decimal baseAmount,
+            [FromQuery] string baseCurrency, [FromQuery] string counterCurrency, [FromQuery] DateTime? date)
+        {
+            if (baseAmount == decimal.Zero)
+                return BadRequest($"{nameof(baseAmount)} must be included in the request, but was not found.");
+
+            if (string.IsNullOrWhiteSpace(baseCurrency))
+                return BadRequest($"{nameof(baseCurrency)} must be included in the request, but was not found.");
+
+            if (string.IsNullOrWhiteSpace(counterCurrency))
+                return BadRequest($"{nameof(counterCurrency)} must be included in the request, but was not found.");
+
+            try
+            {
+                var result = await _currencyService.Convert(new CurrencyPair(baseAmount, baseCurrency, counterCurrency, date));
+                return Ok(result);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                _logger.LogError(exception, exception.Message);
+                return BadRequest(exception.Message);
+            }
+        }
+    }
+}
---
+++ CurrencyConverter.API/Startup.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using CurrencyConvert.Infrastructure.Services;
+using CurrencyConverter.Infrastructure.RelationalStorage;
+
+namespace CurrencyConverter.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddTransient<FixerApiHandler>();
+            services.AddTransient<ICurrencyService, CurrencyService>();
+            services.AddTransient<ExchangeRateDbContext>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
